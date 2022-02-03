948b53309dd05d7d400ffd99c2f343ed601591cf
eksoontop <94976371+eksoontop@users.noreply.github.com>
2022-02-03 18:21:25 +0100

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MythicalHatcher/loader/main/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eksoontop/loader/main/Loader.lua"))()

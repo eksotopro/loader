e268ed3e0ebe74daef1955230fccd946f3c7a723
eksoontop <94976371+eksoontop@users.noreply.github.com>
2022-03-07 18:21:29 +0100

    Create premium.lua

---
+++ premium.lua
@@ -0,0 +1,6 @@
+getgenv().Prefix = "$"
+getgenv().MainID = {2492984718}
+getgenv().AltIDS = {
+    326711113,
+}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/premium.lua'))()

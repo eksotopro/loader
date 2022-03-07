de91791c8a4aabe0c9fd92654fedc6e6a400e602
eksoontop <94976371+eksoontop@users.noreply.github.com>
2022-03-07 19:03:24 +0100

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -6,11 +6,11 @@ getgenv().MainID = {
 }
 getgenv().AltIDS = {
     326711113, --test account dont remove
-	3326019907, --@iceyisnotknown or 856240629122072607
-	2751101759, --@59xBartek or 823890872969461770
-	2536809287, --@26xac or ave:3
-	2244576018, --@ZaCzArOwAnA_JeStEm or ?
-	3240077164, --@eksoziemnlak or ?
-	2851413065, --@StxpidBartek or 936667657934077952
+    3326019907, --@iceyisnotknown or 856240629122072607
+    2751101759, --@59xBartek or 823890872969461770
+    2536809287, --@26xac or ave:3
+    2244576018, --@ZaCzArOwAnA_JeStEm or ?
+    3240077164, --@eksoziemnlak or ?
+    2851413065, --@StxpidBartek or 936667657934077952
 }
 loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/premium.lua'))()
